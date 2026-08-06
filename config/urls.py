05abb540649da4866c2f78b61f12d2a361225e3e
notsuz <ifosuz77@gmail.com>
2026-08-06 15:39:26 +0545

    before commit

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf.urls.static import static 
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,4 +14,4 @@ urlpatterns = [
     
     #API v1
     path('api/v1/auth/', include('apps.accounts.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
